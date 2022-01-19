e9c49bd646d7e1e3b9e96ccc4c1963275df5a0e5
Bonamente <56506035@protonmail.com>
2022-01-19 18:07:26 +0500

    refactor: update types file

--- async-race/src/app/types.ts
+++ async-race/src/app/types.ts
@@ -19,17 +19,53 @@ export type Cars = {
   count: string | null;
 };
 
-export type Sort = 'id' | 'wins' | 'time' | '';
-
-export type Order = 'ASC' | 'DESC' | '';
-
 export type Winner = {
   id: number;
   wins: number;
   time: number;
 };
 
+type Items = { id: number; wins: string; time: string; car: Car }[];
+
 export type Winners = {
-  items: { name: string; color: string; id: number; car: Car }[];
+  items: Items;
   count: string;
 };
+
+export type RaceStatus = {
+  success: boolean;
+  id: number;
+  time: number;
+};
+
+export type RaceResult = {
+  name: string;
+  color: string;
+  id: number;
+  time: number;
+};
+
+export interface IForms {
+  [key: string]: Record<string, string>;
+}
+
+export interface IState {
+  activePage: string;
+
+  forms: IForms;
+
+  carsPage: number;
+  winnersPage: number;
+
+  cars: Car[];
+  carsCount: string | null;
+  selectedCar: Car | null;
+
+  winners: Items;
+  winnersCount: string | null;
+
+  animation: { [key: number]: { id: number } };
+
+  sortBy: string;
+  sortOrder: string;
+}
