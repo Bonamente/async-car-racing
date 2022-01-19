72b99b5ea7c79c8507238dc8bf501eabed7c92d7
Bonamente <56506035@protonmail.com>
2022-01-19 18:18:57 +0500

    fix: handlers logic

--- async-race/src/app/handlers/handlers.ts
+++ async-race/src/app/handlers/handlers.ts
@@ -1,11 +1,22 @@
 import { createCar, getCar, updateCar, deleteCar, deleteWinner, saveWinner } from '../api/api';
-import { IState } from '../types';
+import { IState, RaceStatus } from '../types';
 import startCar from '../utils/start-car';
 import stopCar from '../utils/stop-car';
 import race from '../utils/racing/race';
 import generateRandomCars from '../utils/generate-cars';
-import updateGarage from '../utils/update-garage';
 import renderGarage from '../renders/render-garage';
+import updateGarage from '../utils/update-garage';
+import renderWinners from '../renders/render-winners';
+import updateWinners from '../utils/update-winners';
+import { setSort } from '../utils/set-sort';
+
+export const handleFormInput = (e: Event, state: IState): void => {
+  const inputEl = e.target as HTMLInputElement;
+  const [formName, elName] = inputEl.name.split('-');
+
+  state.forms[formName].processState = 'filling';
+  state.forms[formName][elName] = inputEl.value;
+};
 
 export const handleCreateFormSubmit = async (e: Event, state: IState): Promise<void> => {
   e.preventDefault();
@@ -14,11 +25,19 @@ export const handleCreateFormSubmit = async (e: Event, state: IState): Promise<v
   const colorEl = <HTMLInputElement>document.querySelector('#create-color');
   const car = { name: nameEl.value, color: colorEl.value };
 
+  const raceBtn = <HTMLButtonElement>document.querySelector('.controls__race-btn');
+  raceBtn.disabled = false;
+
   await createCar(car);
   await updateGarage(state);
   renderGarage(state);
 
   nameEl.value = '';
+  colorEl.value = '#ffffff';
+
+  state.forms.create.processState = 'idle';
+  state.forms.create.name = '';
+  state.forms.create.color = '#ffffff';
 };
 
 export const handleUpdateFormSubmit = async (e: Event, state: IState): Promise<void> => {
@@ -34,14 +53,23 @@ export const handleUpdateFormSubmit = async (e: Event, state: IState): Promise<v
   renderGarage(state);
 
   nameEl.value = '';
+  colorEl.value = '#ffffff';
   nameEl.disabled = true;
   colorEl.disabled = true;
   updateBtn.disabled = true;
+
+  state.forms.update.processState = 'idle';
+  state.forms.update.name = '';
+  state.forms.update.color = '#ffffff';
 };
 
 export const handleRaceBtnClick = async (e: Event, state: IState): Promise<void> => {
   const raceBtn = e.target as HTMLButtonElement;
   raceBtn.disabled = true;
+
+  if (state.cars.length === 0) {
+    return;
+  }
 
   const resetBtn = <HTMLButtonElement>document.querySelector('.controls__reset-btn');
   resetBtn.disabled = false;
@@ -58,7 +86,7 @@ export const handleRaceBtnClick = async (e: Event, state: IState): Promise<void>
 
   setTimeout(() => {
     modalEl.classList.add('hidden');
-  }, 7000);
+  }, 5000);
 };
 
 export const handleResetBtnClick = async (e: Event, state: IState): Promise<void> => {
@@ -74,6 +102,9 @@ export const handleResetBtnClick = async (e: Event, state: IState): Promise<void
 export const handleGenerateBtnClick = async (e: Event, state: IState): Promise<void> => {
   const generateBtn = e.target as HTMLButtonElement;
   generateBtn.disabled = true;
+
+  const raceBtn = <HTMLButtonElement>document.querySelector('.controls__race-btn');
+  raceBtn.disabled = false;
 
   const cars = generateRandomCars();
 
@@ -99,6 +130,10 @@ export const handleSelectBtnClick = async (e: Event, state: IState): Promise<voi
   updateCarNameEl.disabled = false;
   updateCarColorEl.disabled = false;
   updateBtn.disabled = false;
+
+  state.forms.update.processState = 'filling';
+  state.forms.update.name = state.selectedCar.name;
+  state.forms.update.color = state.selectedCar.color;
 };
 
 export const handleRemoveBtnClick = async (e: Event, state: IState): Promise<void> => {
@@ -111,18 +146,28 @@ export const handleRemoveBtnClick = async (e: Event, state: IState): Promise<voi
   renderGarage(state);
 };
 
-export const handleStartBtnClick = (e: Event, state: IState) => {
+export const handleStartBtnClick = (e: Event, state: IState): Promise<RaceStatus> => {
   const startBtn = e.target as HTMLButtonElement;
   const [, , id] = startBtn.id.split('-');
 
   return startCar(state, +id);
 };
 
-export const handleStopBtnClick = (e: Event, state: IState) => {
+export const handleStopBtnClick = (e: Event, state: IState): Promise<void> => {
   const stopBtn = e.target as HTMLButtonElement;
   const [, , id] = stopBtn.id.split('-');
 
   return stopCar(state, +id);
+};
+
+export const handleSortBtnClick = (e: Event, state: IState): void => {
+  const sortBtn = e.target as HTMLButtonElement;
+
+  if (sortBtn.classList.contains('table__wins')) {
+    setSort(state, 'wins');
+  } else if (sortBtn.classList.contains('table__time')) {
+    setSort(state, 'time');
+  }
 };
 
 export const handlePrevBtnClick = async (e: Event, state: IState): Promise<void> => {
@@ -137,9 +182,8 @@ export const handlePrevBtnClick = async (e: Event, state: IState): Promise<void>
     case 'winners-page': {
       state.winnersPage -= 1;
 
-      // TODO
-      // await updateWinners(state);
-      // renderWinners(state);
+      await updateWinners(state);
+      renderWinners(state);
       break;
     }
     default:
@@ -159,9 +203,8 @@ export const handleNextBtnClick = async (e: Event, state: IState): Promise<void>
     case 'winners-page': {
       state.winnersPage += 1;
 
-      // TODO
-      // await updateWinners(state);
-      // renderWinners(state);
+      await updateWinners(state);
+      renderWinners(state);
       break;
     }
     default:
